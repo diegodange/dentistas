1ccc6d3aad1ce2e198dfaa703bc836cdcd2acc27
Diego Antoniança <diego.antonianca.ext@espm.br>
2023-09-29 18:03:25 -0300

    Menu com o Recurso de Collapsible

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -31,7 +31,7 @@ class AdminPanelProvider extends PanelProvider
             ->login()
             ->colors([
                 'primary' => Color::Amber,
-            ])->plugin(FilamentProgressbarPlugin::make()->color('#93c47d'))
+            ])->plugin(FilamentProgressbarPlugin::make()->color('#93c47d'))->sidebarCollapsibleOnDesktop()
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
             ->pages([
